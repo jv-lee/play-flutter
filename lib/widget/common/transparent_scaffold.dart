6466615b7850bb8aa899e659f3a6184ade2d895a
lijiawei11 <lijiawei11@baidu.com>
2022-08-09 13:50:02 +0800

    update transparent_scaffold.dart

--- lib/widget/common/transparent_scaffold.dart
+++ lib/widget/common/transparent_scaffold.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 /// @author jv.lee
 /// @date 2022/8/9
-/// @description
+/// @description 透明Scaffold 可主动设置appBar背景颜色适应全屏填充页面 （statusBarIcon根据appBar背景色自动适配颜色）
 class TransparentScaffold extends StatelessWidget {
   final Color? color;
   final Widget child;
