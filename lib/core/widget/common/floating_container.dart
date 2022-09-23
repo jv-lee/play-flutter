40c9092f93b208e1883e6c909defa9af8910e2fa
lijiawei11 <lijiawei11@baidu.com>
2022-09-23 11:03:02 +0800

    add floating_container.dart

---
+++ lib/core/widget/common/floating_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// @author jv.lee
+/// @date 2022/9/22
+/// @description 悬浮容器 支持手势拖动
+class FloatingContainer extends StatefulWidget {
+  const FloatingContainer({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _FloatingContainerState();
+}
+
+class _FloatingContainerState extends State<FloatingContainer> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
