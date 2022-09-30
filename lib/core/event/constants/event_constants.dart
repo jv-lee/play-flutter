09ed9b0294272fabb5227fa4bbfc3c0517a72fc2
lijiawei11 <lijiawei11@baidu.com>
2022-09-30 11:41:19 +0800

    update constants code.

--- lib/core/event/constants/event_constants.dart
+++ lib/core/event/constants/event_constants.dart
@@ -3,9 +3,9 @@
 /// @date 2022/8/15
 /// @description 全局事件常量类
 class EventConstants {
-  // 首页列表重复点击tab回滚至顶部事件
+  /// 首页列表重复点击tab回滚至顶部事件
   static const EVENT_TAB_SELECTED = "event:tab-selected";
 
-  // 登陆token失效，打开登陆页面
+  /// 登陆token失效，打开登陆页面
   static const EVENT_NAVIGATION_LOGIN = "event:navigation-login";
 }
--- lib/core/model/http/constants/api_constants.dart
+++ lib/core/model/http/constants/api_constants.dart
@@ -3,11 +3,11 @@
 /// @date 2022/8/4
 /// @description Api常量类
 class ApiConstants {
-  static const BASE_URI = "https://www.wanandroid.com";
+  static const BASE_URI = "https://www.wanandroid.com"; // 服务地址
   static const REQUEST_OK = 0; // 站点api请求 成功码
   static const REQUEST_TOKEN_ERROR = -1001; // 未登陆 错误码
   static const REQUEST_TOKEN_ERROR_MESSAGE = "TOKEN-ERROR"; // 登陆token失效错误自负
 
-  // 积分规则地址
-  static const URI_COIN_HELP = "https://www.wanandroid.com/blog/show/2653";
+  static const URI_COIN_HELP =
+      "https://www.wanandroid.com/blog/show/2653"; // 积分规则地址
 }
