eef5fc309904798645306b509e522414130561a6
lijiawei11 <lijiawei11@baidu.com>
2023-02-09 10:21:00 +0800

    update details routes theme code.

--- lib/module/details/theme/theme_details.dart
+++ lib/module/details/theme/theme_details.dart
@@ -4,9 +4,10 @@ import 'package:playflutter/core/tools/localizations.dart';
 
 /// @author jv.lee
 /// @date 2023/1/31
-/// @description
+/// @description 详情页模块 资源引用类
 class ThemeDetails {
   static _Strings strings = _Strings();
+  static _Routes routes = _Routes();
 }
 
 class _Strings {
@@ -14,4 +15,8 @@ class _Strings {
   String menuShare = "menu_share".localized();
   String menuCollectCompleted = "menu_collect_completed".localized();
   String menuCollectComplete = "menu_collect_complete".localized();
+}
+
+class _Routes {
+  String details = "/details";
 }
