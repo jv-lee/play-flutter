d33a8d3a0aee5c565f82517945e683ca0c2706ae
lijiawei11 <lijiawei11@baidu.com>
2023-05-10 10:22:44 +0800

    update status_tools.dart code.

--- lib/core/tools/status_tools.dart
+++ lib/core/tools/status_tools.dart
@@ -9,12 +9,12 @@ import 'package:playflutter/core/theme/theme_common.dart';
 /// @date 2020/4/30
 /// @description 状态栏操作工具
 class StatusTools {
-  // 默认系统statusBar or navigationBar 样式设置
+  /// 默认系统statusBar or navigationBar 样式设置
   static defaultSystemBar(BuildContext context) {
     transparentSystemBar(Theme.of(context).brightness);
   }
 
-  // 根据当前主题色设置状态栏导航栏沉浸式
+  /// 根据当前主题色设置状态栏导航栏沉浸式
   static transparentSystemBar(Brightness mode) {
     if (Platform.isAndroid) {
       if (mode == Brightness.dark) {
@@ -35,6 +35,9 @@ class StatusTools {
     }
   }
 
+  /// 更新状态栏 icon颜色
+  /// mode == Brightness.dark == 白色icon
+  /// mode == Brightness.light == 黑色icon
   static updateStatusBarIcon(Brightness mode) {
     if (mode == Brightness.dark) {
       SystemUiOverlayStyle systemUiOverlayStyle =
@@ -47,17 +50,17 @@ class StatusTools {
     }
   }
 
-  // 获取状态栏高度
+  /// 获取状态栏高度
   static getStatusHeight() {
     return MediaQueryData.fromWindow(window).padding.top;
   }
 
-  // 获取navigationBar高度
+  /// 获取navigationBar高度
   static getNavigationHeight() {
     return MediaQueryData.fromWindow(window).padding.bottom;
   }
 
-  // 获取appBarLayoutHeight - 2 校正
+  /// 获取appBarLayoutHeight - 2 校正
   static getAppBarLayoutHeight() {
     return ThemeCommon.dimens.toolbarHeight + getStatusHeight() - 2;
   }
